52559a083a1e329ad48cafcd2a04ea59d72827aa
Bradley Morris <bradley.morris@aaon.com>
2021-06-21 14:33:07 -0500

    2021-06-21 19:33:07.1436019Z - Commit and Push

--- CAP.ps1
+++ CAP.ps1
@@ -3,19 +3,19 @@
     Out-String;
 [String] $GITFetchURL = $GITRemoteShowOrigin.Split("`n")[1].Replace("  Fetch URL: ", "");
 Write-Host $("CAPing to " + $GITFetchURL);
-[String] $CommitMessage = [DateTime]::UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fffffffK") + " (" + $env:COMPUTERNAME + ":" + $env:USERDOMAIN + "\" + $env:USERNAME + ") - {@Action}";
+[String] $CommitMessage = [DateTime]::UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fffffffK") + "{@Identifier} - {@Action}";
 [String] $Output = git status --porcelain=v1 |
     Out-String;
 If ($Output.Length -gt 0)
 {
     $CommitMessage = $CommitMessage.Replace("{@Action}", "Commit and Push");
-    Add-Content -Path "CAP.log" -Value $CommitMessage.Replace("{@Action}", "Commit and Push");
+    Add-Content -Path "CAP.log" -Value $CommitMessage.Replace("{@Identifier}", " (" + $env:COMPUTERNAME + ":" + $env:USERDOMAIN + "\" + $env:USERNAME + ")");
     Add-Content -Path "CAP.log" -Value $Output;
     git add . *>$null;
-    git commit --message $CommitMessage *>$null;
+    git commit --message $CommitMessage.Replace("{@Identifier}", "") *>$null;
     git push origin "master" -q *>$null
 }
 Else
 {
-    Add-Content -Path "CAP.log" -Value $CommitMessage.Replace("{@Action}", "No Changes");
+    Add-Content -Path "CAP.log" -Value $CommitMessage.Replace("{@Action}", "No Changes").Replace("{@Identifier}", " (" + $env:COMPUTERNAME + ":" + $env:USERDOMAIN + "\" + $env:USERNAME + ")");
 }
